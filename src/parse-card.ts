fcfa38497ec6c6333339560f4dc0e415ab4bf50e
IngeniousKink <ingeniouskink@gmail.com>
2024-04-03 04:52:52 +0200

    Simplify

--- src/parse-card.ts
+++ src/parse-card.ts
@@ -9,31 +9,31 @@ interface ParsedCard {
   text?: string;
   annotations?: Annotation[];
 }
+
 const REGEX_HTML_DATA_ATTRIBUTE = /<([^>]+?)>/g;
 const REGEX_DATA_ATTRIBUTE = /data-(\w+)="([^"]+)"/g;
 
+function createAnnotation(dataMatches: RegExpMatchArray[], match: RegExpMatchArray): Annotation {
+  const annotation: Annotation = {};
+  dataMatches.forEach((dataMatch) => {
+    annotation[dataMatch[1]] = dataMatch[2];
+  });
+  annotation.startPos = String(match.index);
+  annotation.endPos = String((match.index || 0) + match[0].length);
+  return annotation;
+}
+
+function parseDataAttributes(match: RegExpMatchArray): Annotation | null {
+  const dataMatches = Array.from(match[1].matchAll(REGEX_DATA_ATTRIBUTE));
+  if (dataMatches.length === 0) return null;
+  return createAnnotation(dataMatches, match);
+}
+
 export function parseHTMLCard(html: string): ParsedCard {
-  const result: ParsedCard = {};
   const matches = Array.from(html.matchAll(REGEX_HTML_DATA_ATTRIBUTE));
-  const annotations: Annotation[] = [];
-  for (const match of matches) {
-    const annotation: Annotation = {};
-    const dataMatches = Array.from(match[1].matchAll(REGEX_DATA_ATTRIBUTE));
-    for (const dataMatch of dataMatches) {
-      if (dataMatch[1] === 'actor') {
-        annotation.actor = dataMatch[2];
-      } else {
-        annotation[dataMatch[1]] = dataMatch[2];
-      }
-    }
-    if (Object.keys(annotation).length > 0) {
-      annotation.startPos = String(match.index);
-      annotation.endPos = String((match.index || 0) + match[0].length);
-      annotations.push(annotation);
-    }
-  }
-  if (annotations.length > 0) {
-    result.annotations = annotations;
-  }
-  return result;
+  const annotations: Annotation[] = matches
+    .map(parseDataAttributes)
+    .filter((annotation): annotation is Annotation => annotation !== null);
+
+  return annotations.length ? { annotations } : {};
 }
