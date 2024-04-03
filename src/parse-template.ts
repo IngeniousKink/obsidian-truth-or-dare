74ac508320708a496a88f5b410b893e4383f6dd8
IngeniousKink <ingeniouskink@gmail.com>
2024-04-03 04:52:52 +0200

    Make regexes even more readable

--- src/parse-template.ts
+++ src/parse-template.ts
@@ -107,48 +107,57 @@ function extractCardsFromParagraph(paragraph: Paragraph, refCounter: number, sta
     const card = {
       ref: stackRef + '^' + refCounter,
       text: textNode.value,
-      ...parseLine(textNode.value),
+      ...parseCard(textNode.value),
     };
     return [...cards, card];
   }, []);
 }
-interface ParsedLine {
+
+interface ParsedCard {
   text?: string;
   [key: string]: string | undefined;
 }
 
-function parseLine(line: string): ParsedLine {
-  const result: ParsedLine = {};
-  const textMatch = line.match(/^(.*?)( \[.+\]| \(.+\)| .+::.*|$)/);
+const REGEX_TEXT_MATCH = 
+    '^' + // Start of the line
+    '(.*?)' + // Capture any character, non-greedy
+    '( \\[.+\\]' + // Matches a space followed by any characters inside square brackets
+    '| \\(.+\\)' + // OR matches a space followed by any characters inside parentheses
+    '| .+::.*' + // OR matches a space followed by any characters until :: and then any characters after
+    '|$)'; // OR end of the line
+
+const REGEX_INLINE_FIELDS = 
+  "(" + // Start of capturing group 1
+    "\\[" + // Match a literal '[' character
+    "|" + // OR
+    "\\(" + // Match a literal '(' character
+  ")" + // End of capturing group 1
+  "(" + // Start of capturing group 2
+    "\\w+" + // Match one or more word characters (a-z, A-Z, 0-9, _)
+  ")" + // End of capturing group 2
+  "::\\ ?" + // Match the string '::' followed by an optional space
+  "(" + // Start of capturing group 3
+    "[^\\]]+" + // Match one or more characters that are not a ']' character
+    "|" + // OR
+    "\\([^\\)]+\\)" + // Match a string that starts with '(' and ends with ')' and has one or more characters that are not a ')' character in between
+    "|" + // OR
+    "[^ ]+" + // Match one or more characters that are not a space character
+  ")" + // End of capturing group 3
+  "(" + // Start of capturing group 4
+    "\\]" + // Match a literal ']' character
+    "|" + // OR
+    "\\)" + // Match a literal ')' character
+  ")"; // End of capturing group 4
+
+function parseCard(line: string): ParsedCard {
+  const result: ParsedCard = {};
+  const textMatch = line.match(new RegExp(REGEX_TEXT_MATCH));
   if (textMatch) {
     result.text = textMatch[1].trim();
   }
-  // Creating a regex pattern as a string to explain each part of it
-  const regexPattern = 
-    "(" + // Start of capturing group 1
-      "\\[" + // Match a literal '[' character
-      "|" + // OR
-      "\\(" + // Match a literal '(' character
-    ")" + // End of capturing group 1
-    "(" + // Start of capturing group 2
-      "\\w+" + // Match one or more word characters (a-z, A-Z, 0-9, _)
-    ")" + // End of capturing group 2
-    "::\\ ?" + // Match the string '::' followed by an optional space
-    "(" + // Start of capturing group 3
-      "[^\\]]+" + // Match one or more characters that are not a ']' character
-      "|" + // OR
-      "\\([^\\)]+\\)" + // Match a string that starts with '(' and ends with ')' and has one or more characters that are not a ')' character in between
-      "|" + // OR
-      "[^ ]+" + // Match one or more characters that are not a space character
-    ")" + // End of capturing group 3
-    "(" + // Start of capturing group 4
-      "\\]" + // Match a literal ']' character
-      "|" + // OR
-      "\\)" + // Match a literal ')' character
-    ")"; // End of capturing group 4
   
   // Creating a regex from the pattern string with global flag
-  const regex = new RegExp(regexPattern, 'g');
+  const regex = new RegExp(REGEX_INLINE_FIELDS, 'g');
   
   // Using the regex to find all matches in the line
   const inlineFieldMatches = line.matchAll(regex);
