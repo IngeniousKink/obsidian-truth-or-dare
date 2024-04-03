0a780c342aaed7d484de13b44fa6bc3c3804fbb7
IngeniousKink <ingeniouskink@gmail.com>
2024-04-03 04:52:54 +0200

    Fix re-rendering of input field

--- src/react/components/ChangeActorNameInput.tsx
+++ src/react/components/ChangeActorNameInput.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { changeActorName, timestampEvent } from '@obsidian-truth-or-dare/events.js';
 import { useDispatchGameEventHook } from '../dispatchEvent.js';
 
@@ -7,26 +7,46 @@ interface ChangeActorNameInputProps {
   name: string;
 }
 
+function debounce<F extends (...args: unknown[]) => unknown>(fn: F, delay: number) {
+  let timeoutID: NodeJS.Timeout | null = null;
+  return (...args: Parameters<F>): void => {
+    if (timeoutID) {
+      clearTimeout(timeoutID);
+    }
+    timeoutID = setTimeout(() => fn(...args), delay);
+  };
+}
+
 export const ChangeActorNameInput: React.FC<ChangeActorNameInputProps> = ({ actorId, name }) => {
+  const [inputValue, setInputValue] = useState(name);
   const useDispatchGameEvent = useDispatchGameEventHook();
   const dispatchGameEvent = useDispatchGameEvent();
 
-  const handleNameChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    const newName = event.target.value;
-
+  const handleNameChange = (newName: string) => {
     if (!actorId || name === newName) return;
 
     const gameEvent = timestampEvent(changeActorName(actorId, newName));
     dispatchGameEvent(gameEvent);
   };
 
+  const debouncedChange = debounce(handleNameChange, 300);
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setInputValue(event.target.value);
+    debouncedChange(event.target.value);
+  };
+
+  const handleBlur = () => {
+    handleNameChange(inputValue);
+  };
+
   return (
     <input
       type="text"
       placeholder="Enter new name"
-      value={name}
-      onChange={handleNameChange}
-      onBlur={handleNameChange}
+      value={inputValue}
+      onChange={handleChange}
+      onBlur={handleBlur}
     />
   );
 };
