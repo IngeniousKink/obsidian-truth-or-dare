af305284276b9b93690fc9dd99fcc301527026b4
IngeniousKink <ingeniouskink@gmail.com>
2024-04-03 04:52:53 +0200

    add vite powered web app that imports one component

---
+++ export/src/App.tsx
@@ -0,0 +1,29 @@
+import React from 'react'
+
+import { useState } from 'react'
+import './App.css'
+import { JsonStringifyOutput } from '../../src/react/components/JsonStringifyOutput.js'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <JsonStringifyOutput javascriptObject={{'a': 1}}/>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.tsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
+}
+
+export default App
