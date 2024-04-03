e8080641e8f1ad2adebbfd1ded2dc302fbd1a534
IngeniousKink <ingeniouskink@gmail.com>
2024-04-03 04:52:52 +0200

    group data attributes of the same html opening tag together

--- src/parse-card.ts
+++ src/parse-card.ts
@@ -9,20 +9,26 @@ interface ParsedCard {
   text?: string;
   annotations?: Annotation[];
 }
+const REGEX_HTML_DATA_ATTRIBUTE = /<([^>]+?)>/g;
+const REGEX_DATA_ATTRIBUTE = /data-(\w+)="([^"]+)"/g;
 
-const REGEX_HTML_DATA_ATTRIBUTE = /data-(\w+)="([^"]+)"/g;
 export function parseHTMLCard(html: string): ParsedCard {
   const result: ParsedCard = {};
   const matches = html.matchAll(REGEX_HTML_DATA_ATTRIBUTE);
   const annotations: Annotation[] = [];
   for (const match of matches) {
     const annotation: Annotation = {};
-    if (match[1] === 'actor') {
-      annotation.actor = match[2];
-    } else {
-      annotation[match[1]] = match[2];
+    const dataMatches = match[1].matchAll(REGEX_DATA_ATTRIBUTE);
+    for (const dataMatch of dataMatches) {
+      if (dataMatch[1] === 'actor') {
+        annotation.actor = dataMatch[2];
+      } else {
+        annotation[dataMatch[1]] = dataMatch[2];
+      }
     }
-    annotations.push(annotation);
+    if (Object.keys(annotation).length > 0) {
+      annotations.push(annotation);
+    }
   }
   if (annotations.length > 0) {
     result.annotations = annotations;
