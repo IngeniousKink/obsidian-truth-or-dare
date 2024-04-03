74f29f28ff82dabba81f15a30a0f1f342f234d46
IngeniousKink <ingeniouskink@gmail.com>
2024-04-03 04:52:51 +0200

    First proof of concept

--- main.ts
+++ main.ts
@@ -1,6 +1,60 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import { Plugin, ItemView, WorkspaceLeaf } from "obsidian";
+
+import { fromMarkdown } from 'mdast-util-from-markdown';
+import type { Root, Code } from "mdast-util-from-markdown/lib";
+
+export const VIEW_TYPE_EXAMPLE = "truth-or-dare-view";
 
 // Remember to rename these classes and interfaces!
+
+
+function getCardsUnderHeading(node : Root) {
+	// debugger;
+
+	let headingText = '';
+
+	const allCardsUnderHeadings = {};
+  
+	node.children.forEach(child => {
+	  let refCounter = 0;
+	  const cardsUnderHeading = [];
+
+	  if (child.type === 'heading' && child.children) {
+	    headingText = child.children[0].value;
+		return;
+ 	  }
+
+	  if (child.type !== 'list') {
+		return;
+	  }
+  
+	  child.children.forEach(listItem => {
+		listItem.children.forEach(paragraph => {
+
+		  paragraph.children.forEach(textNode => {
+			if (textNode.type === 'text') {
+
+			  const filePath = '';
+			  cardsUnderHeading.push({
+				ref:  filePath + '#' + headingText + '^' + refCounter++,
+				text: textNode.value,
+			  });
+			}
+		  });
+		});
+	  });
+
+	  console.log(`Cards under heading "${headingText}":`, cardsUnderHeading);
+
+	  allCardsUnderHeadings[headingText] = cardsUnderHeading;
+
+	});
+  
+	return allCardsUnderHeadings;
+  }
+  
+
+
 
 interface MyPluginSettings {
 	mySetting: string;
@@ -10,125 +64,103 @@ const DEFAULT_SETTINGS: MyPluginSettings = {
 	mySetting: 'default'
 }
 
+
+export class ExampleView extends ItemView {
+	constructor(leaf: WorkspaceLeaf) {
+		super(leaf);
+	}
+
+	getViewType() {
+		return VIEW_TYPE_EXAMPLE;
+	}
+
+	getDisplayText() {
+		return "Truth or Dare";
+	}
+
+	async update() {
+		console.log(new Date().getTime(), "Active leaf changed!")
+
+		const activeFile = this.app.workspace.getActiveFile();
+
+		const { vault } = this.app;
+
+		if (!activeFile) {
+			return;
+		}
+
+		const fileContents: string = await vault.cachedRead(activeFile);
+
+		console.log(fileContents);
+
+		const text = this.app.title + 'xxx';
+
+		const mast = fromMarkdown(fileContents);
+
+		// Assuming your data structure is named 'data'
+		const stacks = getCardsUnderHeading(mast);
+
+		const container = this.containerEl.children[1];
+		container.empty();
+		container.createEl("h3", { text });
+		container.createEl("pre", { text: JSON.stringify(stacks, undefined, 2) });
+
+		// debugger;
+	}
+
+	async onOpen() {
+
+		this.registerEvent(this.app.metadataCache.on("changed", async (file) => {
+			console.log(new Date().getTime(), 'file changed!', file);
+			return this.update();
+		}));
+
+		this.registerEvent(
+			this.app.workspace.on("active-leaf-change", async () => { return this.update(); } )
+		);
+
+		this.update();
+	}
+
+	async onClose() {
+		// Nothing to clean up.
+	}
+}
+
+
 export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
-
 	async onload() {
-		await this.loadSettings();
+		this.registerView(
+			VIEW_TYPE_EXAMPLE,
+			(leaf) => new ExampleView(leaf)
+		);
 
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
+		this.addRibbonIcon("play", "Play truth or dare game", () => {
+			this.activateView();
 		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
-
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
-
-		// This adds a simple command that can be triggered anywhere
-		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
-
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
-				}
-			}
-		});
-
-		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
-
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
-
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
 	}
 
-	onunload() {
-
+	async onunload() {
 	}
 
-	async loadSettings() {
-		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
-	}
+	async activateView() {
+		const { workspace } = this.app;
 
-	async saveSettings() {
-		await this.saveData(this.settings);
+		let leaf: WorkspaceLeaf | null = null;
+		const leaves = workspace.getLeavesOfType(VIEW_TYPE_EXAMPLE);
+
+		if (leaves.length > 0) {
+			// A leaf with our view already exists, use that
+			leaf = leaves[0];
+		} else {
+			// Our view could not be found in the workspace, create a new leaf
+			// in the right sidebar for it
+			leaf = workspace.getRightLeaf(false);
+			await leaf.setViewState({ type: VIEW_TYPE_EXAMPLE, active: true });
+		}
+
+		// "Reveal" the leaf in case it is in a collapsed sidebar
+		workspace.revealLeaf(leaf);
 	}
 }
 
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
-
-	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
-	}
-
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
-	}
-}
-
-class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
-
-	constructor(app: App, plugin: MyPlugin) {
-		super(app, plugin);
-		this.plugin = plugin;
-	}
-
-	display(): void {
-		const {containerEl} = this;
-
-		containerEl.empty();
-
-		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
-			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
-				.onChange(async (value) => {
-					this.plugin.settings.mySetting = value;
-					await this.plugin.saveSettings();
-				}));
-	}
-}
