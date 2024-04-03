a9cb83be331493102d0f0477c9317c589a45e1a3
IngeniousKink <ingeniouskink@gmail.com>
2024-04-03 04:52:52 +0200

    Also parse positions.

--- src/parse-card.ts
+++ src/parse-card.ts
@@ -1,8 +1,8 @@
 interface Annotation {
   actor?: string;
-  startPos?: string;
-  endPos?: string;
   [key: string]: string | undefined;
+  startPos?: string | undefined;
+  endPos?: string | undefined;
 }
 
 interface ParsedCard {
@@ -14,11 +14,11 @@ const REGEX_DATA_ATTRIBUTE = /data-(\w+)="([^"]+)"/g;
 
 export function parseHTMLCard(html: string): ParsedCard {
   const result: ParsedCard = {};
-  const matches = html.matchAll(REGEX_HTML_DATA_ATTRIBUTE);
+  const matches = Array.from(html.matchAll(REGEX_HTML_DATA_ATTRIBUTE));
   const annotations: Annotation[] = [];
   for (const match of matches) {
     const annotation: Annotation = {};
-    const dataMatches = match[1].matchAll(REGEX_DATA_ATTRIBUTE);
+    const dataMatches = Array.from(match[1].matchAll(REGEX_DATA_ATTRIBUTE));
     for (const dataMatch of dataMatches) {
       if (dataMatch[1] === 'actor') {
         annotation.actor = dataMatch[2];
@@ -27,6 +27,8 @@ export function parseHTMLCard(html: string): ParsedCard {
       }
     }
     if (Object.keys(annotation).length > 0) {
+      annotation.startPos = String(match.index);
+      annotation.endPos = String((match.index || 0) + match[0].length);
       annotations.push(annotation);
     }
   }
