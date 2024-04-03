690f5a80033d412cbaa7b2eaeb30a16cb7b0e851
IngeniousKink <ingeniouskink@gmail.com>
2024-04-03 04:52:52 +0200

    simplify

--- src/parse-embeds.ts
+++ src/parse-embeds.ts
@@ -2,18 +2,22 @@ import { PhrasingContent } from "mdast";
 import { Image, Text } from "node_modules/mdast-util-from-markdown/lib/index.js";
 
 function createContentNode<T extends Text | Image>(type: T['type'], content: PhrasingContent, newValue: string, startOffset: number, endOffset: number): T {
-    const commonProps = {
-        position: content.position ? {
+    const commonProps: Partial<T> = {};
+
+    commonProps.type = type;
+
+    if (content.position) {
+        commonProps.position = {
             start: { ...content.position.start, offset: startOffset, column: startOffset + 1 },
             end: { ...content.position.end, offset: endOffset, column: endOffset + 1 }
-        } : undefined
-    };
-
-    if (type === 'image') {
-        return { ...commonProps, type, url: newValue } as Image as T;
+        };
     }
 
-    return { ...commonProps, type, value: newValue } as Text as T;
+    if (type === 'image') {
+        return { ...commonProps, url: newValue } as Image as T;
+    }
+
+    return { ...commonProps, value: newValue } as Text as T;
 }
 
 function processImageLink(
