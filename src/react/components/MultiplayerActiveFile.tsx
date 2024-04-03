0c1f02b84aeb281efe38dc9d8bdf368f92f7b4ef
IngeniousKink <ingeniouskink@gmail.com>
2024-04-03 04:52:54 +0200

    Split multiplayer ui elements

--- src/react/components/MultiplayerActiveFile.tsx
+++ src/react/components/MultiplayerActiveFile.tsx
@@ -1,54 +1,81 @@
-
 import React from 'react';
 import { useMultiplayer } from './useMultiplayer.js';
 
-export const MultiplayerActiveFile = () => {
-  const {
-    publishTemplate,
-    loadEvents,
-    websockets,
-    WEBSOCKET_STATES,
-    loadValue,
-    setLoadValue,
-    seedValue,
-    setSeedValue,
-  } = useMultiplayer();
+export const LoadEntityInput = () => {
+  const { loadValue, setLoadValue } = useMultiplayer();
 
+  return (
+    <label>
+      Load:
+      <input
+        type="text"
+        value={loadValue || ''}
+        onChange={(e) => setLoadValue(e.target.value)}
+      />
+    </label>
+  );
+};
+
+export const SeedInput = () => {
+  const { seedValue, setSeedValue } = useMultiplayer();
+
+  return (
+    <label>
+      Seed:
+      <input
+        type="text"
+        disabled
+        value={seedValue || ''}
+        onChange={(e) => setSeedValue(e.target.value)}
+      />
+    </label>
+  );
+};
+
+export const PublishButton = () => {
+  const { publishTemplate } = useMultiplayer();
+
+  return (
+    <button onClick={publishTemplate}>Publish Template</button>
+  );
+};
+
+export const LoadButton = () => {
+  const { loadEvents } = useMultiplayer();
+
+  return (
+    <button onClick={loadEvents}>Load Template</button>
+  );
+};
+
+export const ConnectionStatus = () => {
+  const { websockets, WEBSOCKET_STATES } = useMultiplayer();
+
+  return (
+    <ul>
+      {websockets.map((ws, index) => (
+        <li key={index}>{ws.url} {WEBSOCKET_STATES[ws.readyState] || 'UNKNOWN'}</li>
+      ))}
+    </ul>
+  );
+};
+
+export const MultiplayerActiveFile = () => {
   return (
     <>
       <fieldset>
         <legend>Multiplayer</legend>
-
-        <label>
-          Load:
-          <input
-            type="text"
-            value={loadValue || ''}
-            onChange={(e) => setLoadValue(e.target.value)}
-          />
-        </label>
+        <LoadEntityInput />
         <br />
-        <label>
-          Seed:
-          <input
-            type="text"
-            disabled
-            value={seedValue || ''}
-            onChange={(e) => setSeedValue(e.target.value)}
-          />
-        </label>
+        <SeedInput />
         <br />
         <br />
-        <button onClick={publishTemplate}>Publish Template</button>
-        <button onClick={loadEvents}>Load Template</button>
+        <PublishButton />
+        <LoadButton />
         <br />
         <br />
         <span>Connection status</span>
-        <ul>
-          {websockets.map((ws, index) => (
-            <li key={index}>{ws.url} {WEBSOCKET_STATES[ws.readyState] || 'UNKNOWN'}</li>
-          ))}
-        </ul>
+        <ConnectionStatus />
       </fieldset>
     </>
   );
